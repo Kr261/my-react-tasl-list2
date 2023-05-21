de1c0e2b973f8a55d1ca46f150613e0b077bd9c9
Kristin Motato <kristinmotato2@gmail.com>
2023-05-21 10:56:13 -0500

    Nuevas funcionalidades con Hooks UseState y UseEffect

---
+++ src/Components/TaskInput.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Task from "./task";
+
+function TaskInput ({ addTask }) {
+
+    const[tittle, setTittle] = useState ("")
+
+    const handleTaskEnter = (e) => {
+        if (e.key.toLowerCase() === "enter") {
+            addTask(tittle)
+            setTittle("")
+        }
+    }
+    const handelTaskButton = (e) => {
+        addTask(tittle)
+        setTittle("")
+     }
+
+    return (
+    <div className="mt-3 relative py-4 flex flex-col items-center"> 
+      <input
+      type="text"
+      name="enviar"
+      className="focus: shadow-lg font-inter focus:shadow-purple-400 bg-purple-100 text-purple-900 pl-12 w-full py-3 rounded-xl outline-none transition-all duration-300 ease-in-out "
+      placeholder="What's your mind focus on today?"
+      value={tittle}
+      onChange={e => setTittle(e.target.value)}
+      onKeyDown={e => handleTaskEnter(e)}
+      />
+
+      <button  onClick={handelTaskButton} type="submit" class="py-2 bg-purple-900 text-purple-50 rounded-lg w-48 mt-3">Agregar tarea</button>
+    </div>
+    )
+}
+
+export default TaskInput;
--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,10 +1,11 @@
 import Task from "./task";
 
 function TaskList ({ task }) {
+
     return (
         <div>
         <h2 className="border-solid border bg-purple-900 text-purple-50 border-purple-900 text-3xl font-bold rounded-lg"> Task List </h2> 
-        <div className="flex flex-col mt-7 rounded-lg overflow-hidden shadow-2xl">
+        <div className="flex flex-col mt-2 rounded-lg overflow-hidden shadow-2xl">
         </div>
         {task.map( task => {
             return (
